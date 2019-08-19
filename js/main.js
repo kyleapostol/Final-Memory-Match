05172a2f1e80939f0cef7c2ee8a4801639e2fc48
Kyle Apostol <kyleapostol@yahoo.com>
2019-08-19 01:11:07 +0000

    created cards dynamically

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-$(document).ready(initialize);
-function initialize(){
+var cardArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 
+$(document).ready(initialize);
+
+function initialize(){
+$('.main').ready(createCards);
 }
 
 function createCards(){
-    
+    for(var i = 0; i < cardArr.length; i++){
+        var card = $("<div></div>").addClass("card col-sm").text(cardArr[i]);
+        $(".main").append(card);
+    }
 }
