71232828ce47a7f194ca68f75448a0c391db0cab
Kyle Apostol <kyleapostol@yahoo.com>
2019-08-19 04:09:59 +0000

    added individual card

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-var cardArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+var cardArr = ["batman.jpg", "captain_America.jpg", "deadpool.png", "flash.jpg", "green_lantern.png",
+             "iron_Man.jpg", "spiderman.jpg", "superman.jpg", "wonder_woman.png"];
+var duplicateCard = null;
 
 $(document).ready(initialize);
-
 function initialize(){
-$('.main').ready(createCards);
+    $('.main').ready(createCards)
 }
 
 function createCards(){
     for(var i = 0; i < cardArr.length; i++){
-        var card = $("<div></div>").addClass("card col-sm").text(cardArr[i]);
+        var card = $("<div></div>").addClass("card col-sm cover").click(handleCardClicked);
+        $(card).css({"background-image": "url(../images/"  +cardArr[i] + ")", "background-size": "cover",
+        "background-position": "center"});
+
         $(".main").append(card);
+        duplicateCard = i;
+
+        var card2 = $("<div></div>").addClass("card col-sm cover").click(handleCardClicked);
+        $(card2).css({"background-image": "url(../images/"  +cardArr[duplicateCard] + ")", "background-size": "cover",
+        "background-position": "center"});
+        
+        $(".main").append(card2);
     }
+}
+
+function handleCardClicked(event){
+    console.log('event: ', event.currentTarget.outerHTML);
 }
