9f3540290ebb0cefa4fe37628bf74b7ea8b45e51
Kyle Apostol <kyleapostol@yahoo.com>
2019-08-22 21:20:31 +0000

    added buttons and finished shuffle button functionality

--- js/main.js
+++ js/main.js
@@ -18,9 +18,11 @@ var isClicked = true;
 $(document).ready(initialize);
 function initialize(){
     $('.main').ready(createCards);
+    $('shuffle-btn').ready(handleBtnClicks);
 }
 
 function shuffle(arr) {
+    console.log("shuffle function ran")
     for (var i = arr.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -28,10 +30,25 @@ function shuffle(arr) {
     return arr;
 }
 
-function createCards(){
+function handleBtnClicks(){
+    console.log("this handleBtnClicks function ran");
+        $(".shuffle-btn").click(()=>{
+            shuffle(cardArr),
+            $(".main").empty();
+            createCards()
+            });
+}
+
+function addHeroSticker(){
+    $(".hero-container").addClass("hero-image");
+}
+
+function createCards(){    
+
     shuffle(cardArr); 
     shuffle(cardArrCopy);
-
+    addHeroSticker();
+    $(".hero-container").css({"background-image":"url('../images')"})
     for(var i = 0; i < cardArr.length; i++){
         duplicateCard = i;
 
@@ -40,8 +57,8 @@ function createCards(){
         var backCard = $("<div></div>").addClass("col-sm");
 
         $(backCard).css({"background-image": "url(../images/"  + cardArr[i] + ")", "background-size": "cover",
-        "background-position": "center", "border": "3px double #F23333"}).addClass("hidden");
-
+        "background-position": "center", "border": "3px double #F23333"});
+// .addClass("hidden")
         $(cardContainer).append(frontCard);
         $(cardContainer).append(backCard);
         $(".main").append(cardContainer);
@@ -51,8 +68,8 @@ function createCards(){
         var backCard2 = $("<div></div>").addClass("col-sm");
         
         $(backCard2).css({"background-image": "url(../images/"  + cardArrCopy[duplicateCard] + ")", "background-size": "cover",
-        "background-position": "center", "border": "3px double #F23333"}).addClass("hidden"); 
-
+        "background-position": "center", "border": "3px double #F23333"}); 
+// .addClass("hidden")
         $(cardContainer2).append(frontCard2);
         $(cardContainer2).append(backCard2);
         $(".main").append(cardContainer2);
