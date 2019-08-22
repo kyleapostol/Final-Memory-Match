bb85fbd207b127cc3ea744865a196e7e7126154f
kyle apostol <kyleapostol@yahoo.com>
2019-08-22 16:36:08 -0700

    last functionality for the buttons

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-var cardArr = ["batman.jpg", "captain_America.jpg", "deadpool.png", "flash.jpg", "green_lantern.png",
-             "iron_Man.jpg", "spiderman.jpg", "superman.jpg", "wonder_woman.png"];
+var cardArr = ["batman3.jpg", "captAmer.jpg", "deadpool.png", "flash.jpg", "aquaman.jpg",
+             "iron_Man.jpg", "spiderman2.jpg", "superman3.jpg", "wolverine.png"];
              
-var cardArrCopy = ["batman.jpg", "captain_America.jpg", "deadpool.png", "flash.jpg", "green_lantern.png",
-"iron_Man.jpg", "spiderman.jpg", "superman.jpg", "wonder_woman.png"]; 
+var cardArrCopy = ["batman3.jpg", "captAmer.jpg", "deadpool.png", "flash.jpg", "aquaman.jpg",
+"iron_Man.jpg", "spiderman2.jpg", "superman3.jpg", "wolverine.png"]; 
 
 var matches = 0;
 var attempts = 0;
+var gamesPlayed = 0;
 var avg = 0;             
 var duplicateCard = null;
 var firstCardClicked = null;
@@ -18,22 +19,34 @@ var isClicked = true;
 $(document).ready(initialize);
 function initialize(){
     $('.main').ready(createCards);
-    $('shuffle-btn').ready(handleBtnClicks);
+    $('shuffle-btn').ready(handleShuffleBtn);
+    $('reset-btn').ready(handleReset);
 }
 
 function shuffle(arr) {
-    console.log("shuffle function ran")
     for (var i = arr.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     return arr;
 }
+function handleReset(){
+    $(".reset-btn").click(()=>{
+        avg = 0;
+        console.log("avg", avg)
+        $('span').text(avg);
+        $('a').text(avg);
+        shuffle(cardArr);
+        $(".main").empty();
+        createCards()
+    });
+}
 
-function handleBtnClicks(){
-    console.log("this handleBtnClicks function ran");
+function handleShuffleBtn(){
         $(".shuffle-btn").click(()=>{
-            shuffle(cardArr),
+            gamesPlayed ++;
+            $('a').text(gamesPlayed);
+            shuffle(cardArr);
             $(".main").empty();
             createCards()
             });
@@ -48,6 +61,7 @@ function createCards(){
     shuffle(cardArr); 
     shuffle(cardArrCopy);
     addHeroSticker();
+
     $(".hero-container").css({"background-image":"url('../images')"})
     for(var i = 0; i < cardArr.length; i++){
         duplicateCard = i;
@@ -68,7 +82,7 @@ function createCards(){
         var backCard2 = $("<div></div>").addClass("col-sm");
         
         $(backCard2).css({"background-image": "url(../images/"  + cardArrCopy[duplicateCard] + ")", "background-size": "cover",
-        "background-position": "center", "border": "3px double #F23333"}); 
+        "background-position": "center", "border": "3px double #F23333"});
 // .addClass("hidden")
         $(cardContainer2).append(frontCard2);
         $(cardContainer2).append(backCard2);
@@ -101,12 +115,22 @@ function handleCardClicked(event){
     }
 }
 
+function appendCharacter(card){
+
+    console.log("string:::::", card.substring(104));
+
+    $(".hero-container").addClass("hero-image");
+};
+
+
+
 function winCondition(firstCardClicked, secondCardClicked){
     isClicked = false;
     attempts++;
     if(firstCardClicked === secondCardClicked) {
         console.log("it matched")
         matches++;
+        appendCharacter(firstCardClicked);
         firstCardClicked = null;
         secondCardClicked = null;
         card1 = null;
@@ -139,7 +163,9 @@ function handleAverage(){
 }
 
 function finishGame(){
-    if(matches === 9){
-    $('.section1').append('<span class="modal">You\'ve Won!</span>');
+    console.log("number of matches: ", matches);
+    if(matches === 2){
+        gamesPlayed ++;
+        $('a').text(gamesPlayed);
     }
 }
