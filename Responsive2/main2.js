de031403a8e5a76c7596c47d84da00fb794036c8
Kyle Apostol <kyleapostol@yahoo.com>
2019-09-04 20:58:10 +0000

    added functionality

--- Responsive2/main2.js
+++ Responsive2/main2.js
@@ -19,20 +19,110 @@ var isClicked = true;
 $(document).ready(initialize);
 
 function initialize(){
-
+createCards();
 };
 
 function createCards() {
-    for(var i = 0; i < cardArr.length; i++){
+    for(var i = 0; i < 8; i++){
         duplicateCard = i;
         
-        var cardContainer =  $("<div></div>").addClass("card-container");
+        var cardContainer =  $("<div></div>").addClass("card-container").click(handleCardClicked);
         var frontCard = $("<div></div>").addClass('front');
-        // var backCard = $("<div></div>");
+        var backCard = $("<div></div>");
 
+        $(backCard).css({"background-image": "url(../images/"  + cardArr[i] + ")", "background-size": "cover",
+        "background-position": "center", "border": "3px groove red", "flex-grow" : 1});
+// .addClass("hidden")
         $(cardContainer).append(frontCard);
-        // $(cardContainer).append(backCard);
+        $(cardContainer).append(backCard);
         $(".top-row").append(cardContainer);
+
+        var cardContainer2 =  $("<div></div>").addClass("card-container").click(handleCardClicked);
+        var frontCard2 = $("<div></div>").addClass('front');
+        var backCard2 = $("<div></div>");
+
+        $(cardContainer2).append(frontCard2);
+        $(cardContainer2).append(backCard2);
+        $(".bottom-row").append(cardContainer2);
+
+
+    }
+}
+
+function handleCardClicked(event){
+    if(isClicked){
+        $(event.currentTarget.childNodes[0]).addClass('hidden');
+        frontCard = $(event.currentTarget.childNodes[1]).removeClass('hidden');
+        $(frontCard).click(event => {event.stopImmediatePropagation()});
+        
+        if( firstCardClicked === null ) {
+            firstCardClicked = event.currentTarget.innerHTML; 
+            console.log("firstCardClicked: ", firstCardClicked);
+            card1 = $(event.currentTarget.childNodes[0]);
+            flippedCard = $(event.currentTarget.childNodes[1]);
+            return firstCardClicked;
+        } else {
+            secondCardClicked = event.currentTarget.innerHTML;   
+            console.log("secondCardClicked: ", secondCardClicked);         
+            card2 = $(event.currentTarget.childNodes[0]);
+            flippedCard2 = $(event.currentTarget.childNodes[1]);
+            winCondition(firstCardClicked, secondCardClicked);
+            firstCardClicked = null;
+        }        
+    }
+}
+
+function winCondition(firstCardClicked, secondCardClicked){
+    isClicked = false;
+    attempts++;
+    if(firstCardClicked === secondCardClicked) {
+        matches++;
+        appendCharacter(firstCardClicked);
+        firstCardClicked = null;
+        secondCardClicked = null;
+        card1 = null;
+        card2 = null;
+        isClicked = true;
+        $('span').text(handleAverage());
+        finishGame();
+    } else {
+        setTimeout(notMatching,1000);
+    }
+}
+
+function notMatching() {
+    $(card1).removeClass('hidden');
+    $(card2).removeClass('hidden');
+    $(flippedCard).addClass('hidden');
+    $(flippedCard2).addClass('hidden');
+    firstCardClicked = null;
+    secondCardClicked = null;
+    card1 = null;
+    card2 = null;
+    isClicked = true;
+}
+
+function handleAverage(){
+    avg = 100 * (matches / attempts);
+    var accNum =avg.toFixed(1).toString();
+    return accNum + '%';
+}
+
+function finishGame(){
+    if(matches === 9) {
+        console.log("finishGame ran");
+        gamesPlayed ++;
+        $('a').text(gamesPlayed);
+        var modal = $("<div></div>").addClass('modal').css({"display" : "block"});
+        var modalMessage = $("<div>You\'ve Won!</div>").addClass('modal-text');
+        var modalBtn = $("<button type='button'></button>").text("Play Again").addClass("btn play-btn btn-success justify-content-center");
+
+        $(modal).append(modalMessage);
+        $(modal).append(modalBtn).click(playAgainBtn);
+        $("body").append(modal);         
+
+        // var finishModal = $(modal).append(playModalBtn);
+        // $("body").addClass("main-opacity");
     }
 }
 // function createCards(){    
